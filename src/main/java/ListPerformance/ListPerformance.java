64cc249671cc2aae68a1b5feed1034e98f892b75
KolodnikovVyacheslav <slava@kolodnikov.com>
2025-04-28 22:37:15 +0300

    Написал класс для сравнения листов

---
+++ src/main/java/ListPerformance/ListPerformance.java
@@ -0,0 +1,86 @@
+package ListPerformance;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListPerformance {
+
+    private final int operationsCount;
+
+    public ListPerformance(int operationsCount) {
+        this.operationsCount = operationsCount;
+    }
+
+    public void testAddPerformance() {
+        System.out.println("Тестируем добавление элементов...");
+
+        List<Integer> arrayList = new ArrayList<>();
+        long startTime = System.nanoTime();
+        for (int i = 0; i < operationsCount; i++) {
+            arrayList.add(i);
+        }
+        long endTime = System.nanoTime();
+        System.out.println("ArrayList - add(" + operationsCount + "): " + (endTime - startTime) + " ns");
+
+        List<Integer> linkedList = new LinkedList<>();
+        startTime = System.nanoTime();
+        for (int i = 0; i < operationsCount; i++) {
+            linkedList.add(i);
+        }
+        endTime = System.nanoTime();
+        System.out.println("LinkedList - add(" + operationsCount + "): " + (endTime - startTime) + " ns");
+    }
+
+    public void testGetPerformance() {
+        System.out.println("Тестируем получение элементов...");
+
+        List<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < operationsCount; i++) {
+            arrayList.add(i);
+        }
+        long startTime = System.nanoTime();
+        for (int i = 0; i < operationsCount; i++) {
+            arrayList.get(i);
+        }
+        long endTime = System.nanoTime();
+        System.out.println("ArrayList - get(" + operationsCount + "): " + (endTime - startTime) + " ns");
+
+        List<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < operationsCount; i++) {
+            linkedList.add(i);
+        }
+        startTime = System.nanoTime();
+        for (int i = 0; i < operationsCount; i++) {
+            linkedList.get(i);
+        }
+        endTime = System.nanoTime();
+        System.out.println("LinkedList - get(" + operationsCount + "): " + (endTime - startTime) + " ns");
+    }
+
+    public void testRemovePerformance() {
+        System.out.println("Тестируем удаление элементов...");
+
+        List<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < operationsCount; i++) {
+            arrayList.add(i);
+        }
+        long startTime = System.nanoTime();
+        for (int i = 0; i < operationsCount; i++) {
+            arrayList.remove(0);
+        }
+        long endTime = System.nanoTime();
+        System.out.println("ArrayList - remove(" + operationsCount + "): " + (endTime - startTime) + " ns");
+
+        List<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < operationsCount; i++) {
+            linkedList.add(i);
+        }
+        startTime = System.nanoTime();
+        for (int i = 0; i < operationsCount; i++) {
+            linkedList.remove(0);
+        }
+        endTime = System.nanoTime();
+        System.out.println("LinkedList - remove(" + operationsCount + "): " + (endTime - startTime) + " ns");
+    }
+}
